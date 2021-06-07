718524c674d7674dc4834952ada9835391ca358f
thomasraynal98 <thomasraynal98@icloud.com>
2021-06-07 11:37:58 +0200

    [MEDIUM] improve CML for OPENCV

--- core/rane_software.cpp
+++ core/rane_software.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
 #include "../include/fonction.h"
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
+    // Read the image file
+    Mat image = imread("../data/test/DEVO.jpg");
+
+    // Check for failure
+    if (image.empty()) 
+    {
+    cout << "Could not open or find the image" << endl;
+    cin.get(); //wait for any key press
+    return -1;
+    }
+
+    String windowName = "The Guitar"; //Name of the window
+
+    namedWindow(windowName); // Create a window
+
+    imshow(windowName, image); // Show our image inside the created window.
+
+    waitKey(0); // Wait for any keystroke in the window
+
+    destroyWindow(windowName); //destroy the created window
+
 }
